1af25adb7a996ae89a39f66d95d357ecd2c1abca
yasuda <eriko@csplan.jp>
2025-06-11 20:17:11 +0900

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import problems.practice;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    // 例: 問題1-1を実行したい場合
+    String result = practice.exec(System.in);
+    System.out.print(result);
+  }
+}
---
+++ src/main/java/problems/practice.java
@@ -0,0 +1,17 @@
+package problems;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class practice {
+  public static String exec(InputStream in) throws IOException {
+    Scanner sc = new Scanner(in);
+    String line = sc.nextLine();
+    String result = line;
+
+    return result + "\n";
+  }
+}
